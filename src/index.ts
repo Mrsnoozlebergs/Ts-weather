b7a64be13940b904427d9ed9dfc3947526664ab6
灀洲 <shuangzhou.wh@alibaba-inc.com>
2020-10-19 15:24:24 +0800

    add the commander

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import commander from 'commander';
+
+commander
+    .version("0.1.0")
+    .option("-p, --peppers", "Add peppers")
+    .option("-P, --pineapple", "Add pineapple")
+    .option("-b, --bbq-sauce", "Add bbq sauce")
+    .option("-c, --cheese [type]", "Add the specified type of cheese [marble]", "marble")
+    .parse(process.argv)
